44f21b9a1c0b5a7dbc4d575016e8f2b65bdf611b
Christian <lvchristian20@gmail.com>
2026-01-20 19:15:12 -0500

    docs(exception): add Swagger/OpenAPI annotations to `ApiError` for enhanced API documentation

--- src/main/java/com/christn/salesinventoryapi/exception/ApiError.java
+++ src/main/java/com/christn/salesinventoryapi/exception/ApiError.java
@@ -1,13 +1,26 @@
 package com.christn.salesinventoryapi.exception;
 
+import io.swagger.v3.oas.annotations.media.Schema;
+
 import java.time.LocalDateTime;
 
 public record ApiError(
+        @Schema(description = "URL del tipo de error", example = "https://christn.com/errors/404")
         String type,
+
+        @Schema(description = "Título del error", example = "Not Found")
         String title,
+
+        @Schema(description = "Código de estado HTTP", example = "404")
         int status,
+
+        @Schema(description = "Detalles del error", example = "El producto con ID 4 no se pudo encontrar")
         String detail,
+
+        @Schema(description = "Ruta donde ocurrió el error", example = "/api/products/4")
         String instance,
+
+        @Schema(description = "Fecha y hora del error")
         LocalDateTime timestamp
 ) {
 }
