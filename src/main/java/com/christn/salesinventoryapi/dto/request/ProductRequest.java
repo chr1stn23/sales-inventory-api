ea31184449b2fb6d153d68caa01a69e99badb436
Christian <lvchristian20@gmail.com>
2026-01-23 15:49:28 -0500

    feat(product): add custom validation messages in ProductRequest DTO
    - Updated validation annotations to include custom error messages for better feedback:
      - `@NotBlank` for name.
      - `@Positive` for price.
      - `@Min` for stock.
      - `@NotNull` for categoryId.

--- src/main/java/com/christn/salesinventoryapi/dto/request/ProductRequest.java
+++ src/main/java/com/christn/salesinventoryapi/dto/request/ProductRequest.java
@@ -8,7 +8,7 @@ import java.math.BigDecimal;
 public record ProductRequest(
 
         @Schema(description = "Nombre del producto", example = "Televisor", requiredMode = Schema.RequiredMode.REQUIRED)
-        @NotBlank
+        @NotBlank(message = "El nombre no puede estar vacío")
         @Size(max = 150)
         String name,
 
@@ -19,18 +19,18 @@ public record ProductRequest(
         @Schema(description = "Precio del producto", example = "1000.00", requiredMode = Schema.RequiredMode.REQUIRED
                 , minimum = "0", exclusiveMinimum = true)
         @NotNull
-        @Positive
+        @Positive(message = "El precio debe ser mayor que 0")
         BigDecimal price,
 
         @Schema(description = "Cantidad en stock del producto", example = "10", requiredMode =
                 Schema.RequiredMode.REQUIRED, minimum = "0")
         @NotNull
-        @Min(0)
+        @Min(value = 0, message = "El stock no puede ser negativo")
         Integer stock,
 
         @Schema(description = "Identificador de la categoría del producto", example = "1", requiredMode =
                 Schema.RequiredMode.REQUIRED)
-        @NotNull
+        @NotNull(message = "El id de categoría no puede ser nulo")
         Long categoryId
 ) {
 }
