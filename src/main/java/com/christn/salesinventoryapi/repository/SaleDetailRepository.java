13b13c2af3d73bc5dcb1ad04c5f3caca3ed5db85
Christian <lvchristian20@gmail.com>
2026-01-23 21:29:46 -0500

    refactor(repository): remove unused SaleDetailRepository interface

--- src/main/java/com/christn/salesinventoryapi/repository/SaleDetailRepository.java
+++
@@ -1,7 +0,0 @@
-package com.christn.salesinventoryapi.repository;
-
-import com.christn.salesinventoryapi.model.SaleDetail;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface SaleDetailRepository extends JpaRepository<SaleDetail, Long> {
-}
